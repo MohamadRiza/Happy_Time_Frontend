ada50c00cc58887e1b3b98725430e0d2b5716378
MohamadRiza <rawufdeenriza@gmail.com>
2025-12-19 20:12:27 +0530

    Aboutpage maps

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -10,7 +10,8 @@ const branches = [
     phone: '+94 77 123 4567',
     image:
       'https://lh3.googleusercontent.com/gps-cs-s/AG0ilSy6UDynoIaGrBBe5auk2SVB2B-uXWf0Y6lYYBYbYHgfGw37o3DkPW7PCwCblIwyoydTk3UOal3RS-EreuI8ISWdVUbUc7Fh5zKqa47Y79BDckNRGkBCocD3PYtInikpi63J3c6NJVyw5Ik=s680-w680-h510',
-    mapLink: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.611869407306!2d79.84851167406612!3d6.936905018223155!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae259261ada6aad%3A0x64dff49a1c0ccff2!2sHappy%20Time%20(Pvt)%20Ltd%20-%20Colombo%2011!5e0!3m2!1sen!2slk!4v1766154179525!5m2!1sen!2slk',
+    mapLink:
+      'https://www.google.com/maps?q=Happy+Time+(Pvt)+Ltd,+Pettah,+Colombo&output=embed',
   },
   {
     id: 2,
@@ -18,8 +19,9 @@ const branches = [
     address: 'No 143, 2nd Cross Street, Pettah, Colombo, Sri Lanka',
     phone: '+94 71 987 6543',
     image:
-      'https://lh3.googleusercontent.com/gps-cs-s/AG0ilSy6UDynoIaGrBBe5auk2SVB2B-uXWf0Y6lYYBYbYHgfGw37o3DkPW7PCwCblIwyoydTk3UOal3RS-EreuI8ISWdVUbUc7Fh5zKqa47Y79BDckNRGkBCocD3PYtInikpi63J3c6NJVyw5Ik=s680-w680-h510',
-    mapLink: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.611869407306!2d79.84851167406612!3d6.936905018223155!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae259261ada6aad%3A0x64dff49a1c0ccff2!2sHappy%20Time%20(Pvt)%20Ltd%20-%20Colombo%2011!5e0!3m2!1sen!2slk!4v1766154179525!5m2!1sen!2slk',
+      'https://lh3.googleusercontent.com/p/AF1QipPCaSrRY-KInMSCHCAVmckf46xC4ASBekS6FeGR=s680-w680-h510',
+    mapLink:
+      'https://www.google.com/maps?q=No+143,+2nd+Cross+Street,+Pettah,+Colombo,+Sri+Lanka&output=embed',
   },
   {
     id: 3,
@@ -27,8 +29,9 @@ const branches = [
     address: 'No 84, 2nd Cross Street, Pettah, Colombo, Sri Lanka',
     phone: '+94 75 456 7890',
     image:
-      'https://images.pexels.com/photos/998245/pexels-photo-998245.jpeg',
-    mapLink: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.611869407306!2d79.84851167406612!3d6.936905018223155!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae259261ada6aad%3A0x64dff49a1c0ccff2!2sHappy%20Time%20(Pvt)%20Ltd%20-%20Colombo%2011!5e0!3m2!1sen!2slk!4v1766154179525!5m2!1sen!2slk',
+      'https://lh3.googleusercontent.com/gps-cs-s/AG0ilSx3VTjZPG1WzVFKWAOlfELllTrBAZF3xGC2lwWQWwMQOWMCbiDIRpd77aLZQuHBkiCvsuDz95jyfDUnuGFrEmJ4jjY__wBEXzoAUd_NxRZ18ILihq23rALg_rrFVtUdhIoK7EtA6A=s680-w680-h510',
+    mapLink:
+      'https://www.google.com/maps?q=No+84,+2nd+Cross+Street,+Pettah,+Colombo,+Sri+Lanka&output=embed',
   },
   {
     id: 4,
@@ -37,7 +40,8 @@ const branches = [
     phone: '+94 77 654 3210',
     image:
       'https://images.pexels.com/photos/190819/pexels-photo-190819.jpeg',
-    mapLink: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.611869407306!2d79.84851167406612!3d6.936905018223155!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae259261ada6aad%3A0x64dff49a1c0ccff2!2sHappy%20Time%20(Pvt)%20Ltd%20-%20Colombo%2011!5e0!3m2!1sen!2slk!4v1766154179525!5m2!1sen!2slk',
+    mapLink:
+      'https://www.google.com/maps?q=No+57,+Yatinuwara+Lane,+Kandy,+Sri+Lanka&output=embed',
   },
   {
     id: 5,
@@ -47,12 +51,14 @@ const branches = [
     phone: '+971 55 123 4567',
     image:
       'https://images.pexels.com/photos/4388167/pexels-photo-4388167.jpeg',
-    mapLink: 'https://www.google.com/maps?q=No.102-104,+Murshid+Bazar,+Deira,+Dubai,+UAE&output=embed',
+    mapLink:
+      'https://www.google.com/maps?q=No.102-104,+Murshid+Bazar,+Deira,+Dubai,+UAE&output=embed',
   },
 ];
 
 const AboutPage = () => {
   const [selectedBranch, setSelectedBranch] = useState(null);
+  const [mapLoaded, setMapLoaded] = useState(false);
 
   return (
     <div className="bg-black text-white min-h-screen">
@@ -115,8 +121,11 @@ const AboutPage = () => {
           <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
             {branches.map((branch) => (
               <button
-                key={branch.id} 
-                onClick={() => setSelectedBranch(branch)}
+                key={branch.id}
+                onClick={() => {
+                  setSelectedBranch(branch);
+                  setMapLoaded(false); // reset loading state
+                }}
                 className="border border-gray-800 rounded-2xl overflow-hidden hover:border-gold transition-all duration-300 block text-left"
               >
                 <img
@@ -145,7 +154,7 @@ const AboutPage = () => {
 
         {/* MODAL FOR MAP */}
         {selectedBranch && (
-          <div className="fixed inset-0 bg-black/70 flex items-center justify-center z-50">
+          <div className="fixed inset-0 bg-black/70 flex items-center justify-center z-50 px-4">
             <div className="bg-gray-900 rounded-2xl p-4 max-w-3xl w-full relative">
               <button
                 onClick={() => setSelectedBranch(null)}
@@ -156,14 +165,20 @@ const AboutPage = () => {
               <h3 className="text-gold text-2xl font-semibold mb-4 text-center">
                 {selectedBranch.name}
               </h3>
-              <div className="w-full h-96">
+              <div className="w-full h-[60vh] relative">
+                {!mapLoaded && (
+                  <div className="absolute inset-0 flex items-center justify-center z-20">
+                    <span className="text-white">Loading map...</span>
+                  </div>
+                )}
                 <iframe
                   src={selectedBranch.mapLink}
                   width="100%"
                   height="100%"
-                  className="rounded-xl"
+                  className="rounded-xl relative z-10"
                   allowFullScreen
                   loading="lazy"
+                  onLoad={() => setMapLoaded(true)}
                   title={selectedBranch.name}
                 ></iframe>
               </div>
