a0bf3b576ee0ac4c9e6072d4b798be08079aea31
MohamadRiza <rawufdeenriza@gmail.com>
2026-02-13 21:27:12 +0530

    Login BTN if Customer is not logged in yet

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -567,6 +567,19 @@ const Navbar = () => {
               <span className="mt-1">Contact</span>
             </Link>
 
+                {/* SHOWS WHEN CUSTOMER IS NOT LOGGED IN YET LOGIN BUTTON CHANGED 2132026/ */}
+            {authType === 'guest' && (
+              <Link
+                to="/login"
+                className="flex flex-col items-center text-xs text-gray-400 hover:text-gold transition-all duration-300 transform hover:scale-110 active:scale-95"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
+                </svg>
+                <span className="mt-1">Login</span>
+              </Link>
+            )}
+
             {authType === 'customer' && (
               <>
                 <Link
